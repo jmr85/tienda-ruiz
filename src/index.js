c3c3bae1e98743515b67d67ff9a90270e565640f
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-07-22 22:24:41 -0300

    semantic-ui mas index.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 const App = () => {
   return (
-    <div>
+    <div id="nueva">
       <h2 class="ui center aligned icon header">
         <i class="circular users icon"></i>
         Curso de React
@@ -13,21 +14,29 @@ const App = () => {
           <div class="item">
             <div class="content">
               <div class="header">Uno</div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
             </div>
           </div>
           <div class="item">
             <div class="content">
               <div class="header">Dos</div>
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat
             </div>
           </div>
           <div class="item">
             <div class="content">
               <div class="header">Tres</div>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
             </div>
           </div>
           <div class="item">
             <div class="content">
               <div class="header">Cuatro</div>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur
             </div>
           </div>
         </div>
