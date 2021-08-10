92ce2eac2943775c965b79e23fc29aa82c20d8bf
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-08-10 09:52:59 -0300

    add CardGroup and delete ListGroup

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { ListGroup, Card } from 'react-bootstrap';
+import { CardGroup } from 'react-bootstrap';
 import ItemList from './ItemList';
 
 
 const ItemListContainer = ({greeting}) => {
     return (
-        <div style={divStyle}>
-                <Card style={{ width: '18rem' }}>
-                <ListGroup variant="flush">
-                    <ListGroup.Item>
-                        <ItemList/>
-                    </ListGroup.Item>
-                </ListGroup>
-            </Card>
+        <div style={divStyle}>        
+            <CardGroup>
+                <ItemList/>
+            </CardGroup>      
         </div>
     )
 }
