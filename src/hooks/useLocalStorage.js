63544911f429b0f361957556d9935609ab1fd4a4
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2021-09-21 07:47:49 -0300

    PRE Entrega Proyecto Final 11 -> useLocalStorage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+
+const useLocalStorage = (defaultValue, key) => {
+    const [value, setValue] = useState(() => {
+        if (typeof window !== "undefined") {
+            const saved = window.localStorage.getItem(key);
+            return saved !== null
+                ?
+                JSON.parse(saved)
+                :
+                defaultValue
+        }
+    });
+
+    useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+
+    return [value, setValue];
+}
+
+export default useLocalStorage;
